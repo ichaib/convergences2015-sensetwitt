4c6762d8d9b8f5157af76e2b5769c1dc0e76046e
Sebastien Bourdu <github@ekkans.com>
2012-10-04 14:22:54 -0400

    Basic setup for imwithyunus

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,10 +3,7 @@
 module PagesHelper
 
   def title
-    html = "Avec "
-    html << '<a href="http://www.convergences2015.org/" target="_blank">Convergence2015</a>'
-    html << ", soutiens les objectifs du millénaire en tweetant !"
-    html.html_safe
+    html = "I am with Yunus."
   end
 
 end
