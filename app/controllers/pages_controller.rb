7d0f82e16c9976e17aac1f82359a2b1e694f1b96
Sebastien Bourdu <github@ekkans.com>
2012-10-09 00:46:33 -0400

    Almost ready for launch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,14 +9,22 @@ class PagesController < ApplicationController
     require 'uri'
 
     @deputies = Deputy.all
-    @twitter_message = URI.escape Proposal.find(params[:proposal_id]).twitter_message
+
+    if 1 == 1 # only one proposition
+      @proposal = Proposal.first
+      @twitter_message = URI.escape @proposal.twitter_message
+    else
+      @twitter_message = URI.escape Proposal.find(params[:proposal_id]).twitter_message
+    end
   end
 
   def thanks
     require 'uri'
-    
-    @influencers = Influencer.all
-    @twitter_message = URI.escape Proposal.find(params[:proposal_id]).twitter_message
+
+    if 1 == 2
+      @influencers = Influencer.all
+      @twitter_message = URI.escape Proposal.find(params[:proposal_id]).twitter_message
+    end
 
     if params[:type] == "politicien"
       sent_message = SentMessage.find_or_create_by_deputy_id_and_proposal_id :deputy_id => params[:person_id], :proposal_id => params[:proposal_id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Solitweet::Application.routes.draw do
 
-  resources :sent_messages
-
-  root :to => 'pages#home'
+  root :to => 'pages#deputies' # no home because only one proposition
   
   resources :deputies
   resources :influencers
   resources :proposals
+  resources :sent_messages
 
-  match "choix/:proposal_id" => "pages#deputies", :as => :choice
+  # match "choix/:proposal_id" => "pages#deputies", :as => :choice
   match "merci/(:proposal_id/:type/:person_id)" => "pages#thanks", :as => :thanks
   match "sense-hackers" => "pages#sense_hackers", :as => :sense_hackers
   match "a-propos" => "pages#about", :as => :about
