210a21cbb55a23cf953e5bd2c7d5b3222054bd9f
Sebastien Bourdu <github@ekkans.com>
2012-10-09 00:55:34 -0400

    Changed 'thank you' url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,15 +24,15 @@ class PagesController < ApplicationController
     if 1 == 2
       @influencers = Influencer.all
       @twitter_message = URI.escape Proposal.find(params[:proposal_id]).twitter_message
-    end
 
-    if params[:type] == "politicien"
-      sent_message = SentMessage.find_or_create_by_deputy_id_and_proposal_id :deputy_id => params[:person_id], :proposal_id => params[:proposal_id]
-    elsif params[:type] == "influenceur"
-      sent_message = SentMessage.find_or_create_by_influencer_id_and_proposal_id :influencer_id => params[:person_id], :proposal_id => params[:proposal_id]
+      if params[:type] == "politicien"
+        sent_message = SentMessage.find_or_create_by_deputy_id_and_proposal_id :deputy_id => params[:person_id], :proposal_id => params[:proposal_id]
+      elsif params[:type] == "influenceur"
+        sent_message = SentMessage.find_or_create_by_influencer_id_and_proposal_id :influencer_id => params[:person_id], :proposal_id => params[:proposal_id]
+      end
+      sent_message.count += 1
+      sent_message.save
     end
-    sent_message.count += 1
-    sent_message.save
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Solitweet::Application.routes.draw do
   resources :sent_messages
 
   # match "choix/:proposal_id" => "pages#deputies", :as => :choice
-  match "merci/(:proposal_id/:type/:person_id)" => "pages#thanks", :as => :thanks
+  match "thank-you" => "pages#thanks", :as => :thanks
   match "sense-hackers" => "pages#sense_hackers", :as => :sense_hackers
   match "a-propos" => "pages#about", :as => :about
   
